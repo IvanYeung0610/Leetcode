d8c80cf3e299695891829795c2df9871f7fd3c87
IvanYeung0610 <ivanyeung0610@gmail.com>
2026-06-08 19:56:57 -0400

    Completed 150 Evaluate Reverse Polish Notation for Stack topic.

---
+++ stack/150_evaluate_reverse_polish_notation.py
@@ -0,0 +1,43 @@
+"""
+Ivan Yeung
+
+150. Evaluate Reverse Polish Notation
+You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+Evaluate the expression. Return an integer that represents the value of the expression.
+
+Note that:
+
+    The valid operators are '+', '-', '*', and '/'.
+    Each operand may be an integer or another expression.
+    The division between two integers always truncates toward zero.
+    There will not be any division by zero.
+    The input represents a valid arithmetic expression in a reverse polish notation.
+    The answer and all the intermediate calculations can be represented in a 32-bit integer.
+"""
+import math
+
+class Solution:
+    def evalRPN(self, tokens: list[str]) -> int:
+        stack: list[int] = []
+        for token in tokens:
+            if token == '+':
+                second = stack.pop()
+                first = stack.pop()
+                stack.append(first + second)
+            elif token == '-':
+                second = stack.pop()
+                first = stack.pop()
+                stack.append(first - second)
+            elif token == '*':
+                second = stack.pop()
+                first = stack.pop()
+                stack.append(first * second)
+            elif token == '/':
+                second = stack.pop()
+                first = stack.pop()
+                stack.append(math.trunc(first / second))
+            else:
+                stack.append(int(token))
+
+        return stack.pop()

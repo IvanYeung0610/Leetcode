d76fafe0e720fafa62bf58700e7e23d159436905
IvanYeung0610 <ivanyeung0610@gmail.com>
2026-06-25 16:25:39 -0400

    Completed 4 Median of Two Sorted Arrays while referencing the solution from NeetCode for the topic Binary Search.

--- binary-search/4_median_of_two_sorted_arrays.py
+++ binary-search/4_median_of_two_sorted_arrays.py
@@ -19,10 +19,47 @@ class Solution:
         left = 0
         right = len(nums1) - 1
 
-        while left <= right:
+        while True:
             # the index we are taking up to from the smaller array
             mid = left + ((right - left) // 2)
             # the index we are taking up to from the larger array
-            larger_index = half - (mid + 2)
+            remaining = half - (mid + 2)
 
-            if ()
+            # Set up variables for partition boundary of smaller array
+            if mid >= 0:
+                nums1Left = nums1[mid]
+            else:
+                nums1Left = float("-infinity")
+
+            if (mid + 1) < len(nums1):
+                nums1Right = nums1[mid + 1]
+            else:
+                nums1Right = float("infinity")
+
+            # Set up variables for partition boundary of larger array
+            if remaining >= 0:
+                nums2Left = nums2[remaining]
+            else:
+                nums2Left = float("-infinity")
+
+            if (remaining + 1) < len(nums2):
+                nums2Right = nums2[remaining + 1]
+            else:
+                nums2Right = float("infinity")
+
+            # caculate median once valid partitions have been found
+            if nums1Left <= nums2Right and nums2Left <= nums1Right:
+                # case where the median is just middle number since total is odd
+                if total % 2 != 0:
+                    return min(nums1Right, nums2Right)
+                # case where average needs to be taken since total is even
+                return (max(nums1Left, nums2Left) + min(nums1Right, nums2Right)) / 2
+
+            # Binary Search Adjustments
+            elif nums1Left > nums2Right:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return 0
+
